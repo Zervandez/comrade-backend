d19073f793d1e9135fef62dfdedef0d827e82d23
Zervandez <arielhalilaj@live.com>
2022-06-09 18:29:24 +0200

    offset

--- src/handlers/addUser.ts
+++ src/handlers/addUser.ts
@@ -1,5 +1,6 @@
 import {insert} from '../repository/user.ts'
 
-export function addUser(){
-    
+export async function addUser({response, request}:any){
+    console.log(request);
+    await response.body('ADDING USER');
 }
--- src/repository/user.ts
+++ src/repository/user.ts
@@ -8,13 +8,16 @@ export async function search(params = {}) {
 
 interface InsertParams{
     name : string,
+    password : string,
+    mail : string,
+    city : string,
     age : number,
     sex : string 
 }
 
-export async function insert({name, age, sex}: InsertParams) {
+export async function insert({name, password, mail, city, age, sex}: InsertParams) {
     // @slavic, change gender => sex
-    return await client.execute('INSERT INTO user(name, age, gender) VALUES(?,?)', [name, age, sex]);
+    return await client.execute('INSERT INTO user(name, password, mail, city, age, sex) VALUES(?,?,?,?,?,?)', [name, password, mail, city, age, sex]);
 }
 
 export function update() {
