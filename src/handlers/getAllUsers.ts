0bc305300e7df9f96e7dcda5db1367b0666cc12c
Zervandez <arielhalilaj@live.com>
2022-06-09 09:34:49 +0200

    response test

--- src/handlers/getAllUsers.ts
+++ src/handlers/getAllUsers.ts
@@ -1,11 +1,14 @@
 import client from "../db.ts";
 
 
+// deno-lint-ignore no-explicit-any
 export async function getAllUsers(ctx: any) {
   try {
     const users = await client.query("SELECT * FROM user");
     console.log(users);
     ctx.response.body = users;
+    const r = new Response();
+    r.body;
   } catch (error) {
     console.log(error);
   }
